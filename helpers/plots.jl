1447342d71cd9f1b45c4b642025b0cf1e9123b52
Denis Vlašiček <denis.vlasicek@gmail.com>
2022-06-20 22:45:17 +0200

    add 1st comparison - mutilated and independent

--- helpers/plots.jl
+++ helpers/plots.jl
@@ -4,8 +4,8 @@ function compare_yhat(d_comparisons;
                       y_pred_1_name = "Y_dag",
                       y_pred_2_name = "Y_bn")
 
-    p = StatsPlots.scatter(d_comparisons[:, y_pred_1_name],
-                           d_comparisons[:, y_pred_2_name];
+    p = StatsPlots.scatter(d_comparisons[!, y_pred_1_name],
+                           d_comparisons[!, y_pred_2_name];
                            legend = false,
                            markercolor = :black,
                            markeralpha = 0.3,
