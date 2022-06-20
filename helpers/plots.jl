b77b975e835e6dc0c54544f8e6e808870fa8102d
Denis Vlašiček <denis.vlasicek@gmail.com>
2022-06-20 10:49:31 +0200

    mv comparison plots to fn. switch to cauchy for prior switch check

---
+++ helpers/plots.jl
@@ -0,0 +1,22 @@
+using StatsPlots
+
+function compare_yhat(d_comparisons;
+                      y_pred_1_name = "Y_dag",
+                      y_pred_2_name = "Y_bn")
+
+    p = StatsPlots.scatter(d_comparisons[:, y_pred_1_name],
+                           d_comparisons[:, y_pred_2_name];
+                           legend = false,
+                           markercolor = :black,
+                           markeralpha = 0.3,
+                           markerstrokecolor = nothing,
+                           xlabel = y_pred_1_name,
+                           ylabel = y_pred_2_name)
+
+    StatsPlots.plot!(p,
+                     x -> x;
+                     linecolor = :red,
+                     linestyle = :dash)
+
+    return p
+end
